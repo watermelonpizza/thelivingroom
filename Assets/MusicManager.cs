40df6847c84b9c776ae3d38a5e354bb1fd2cf3b2
JeremyBurns <soundconclusion@gmail.com>
2019-01-26 10:48:54 +1100

    Music Draft and Music Manager script
     Created a script to manage playing intro tracks followed by loops. Added a draft main title track and loop.

---
+++ Assets/MusicManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+    public AudioSource audioSource;
+   // public AudioClip introTrack;
+    public AudioClip loopTrack;
+
+   // private bool isPlaying;
+
+    // Use this for initialization
+    void Start ()
+    {
+        audioSource = GetComponent<AudioSource>();
+                     
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (!audioSource.isPlaying)
+        {
+            audioSource.clip = loopTrack;
+            audioSource.Play();
+            audioSource.loop = isActiveAndEnabled;
+        }
+	}
+}
