6aece8152f85b5fa1ac94ab31422c90059d4bd53
Daniel Miller <watermelonpizza@melonstudios.net>
2019-01-26 21:32:07 +1100

    Add attack box

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,13 +6,17 @@ using UnityEngine.AI;
 
 //[RequireComponent(typeof(Animator))]
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
 public class Player : MonoBehaviour
 {
     public PlayerSettings settings;
+    public GameObject playerAttackPrefab;
 
     private Animator _animator;
     private Rigidbody2D _rigidbody2D;
+    private BoxCollider2D _boxCollider2D;
     private Vector2 _lastMovement = Vector2.down;
+    private Vector2 _lastDirection = Vector2.down;
     private float _lastFire = 0;
 
     [HideInInspector]
@@ -22,11 +26,13 @@ public class Player : MonoBehaviour
     {
         //_animator = GetComponent<Animator>();
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _boxCollider2D = GetComponent<BoxCollider2D>();
     }
 
     private void Update()
     {
-        Vector2 movement = new Vector2(Input.GetAxis("Horizontal") * settings.speed, Input.GetAxis("Vertical") * settings.speed);
+        var movement = new Vector2(Input.GetAxis("Horizontal") * settings.speed, Input.GetAxis("Vertical") * settings.speed);
+        var direction = GetDirection(movement);
 
         //_animator.SetFloat("Horizontal", movement.x);
         //_animator.SetFloat("Vertical", movement.y);
@@ -37,19 +43,88 @@ public class Player : MonoBehaviour
         if (Input.GetAxis("Jump") > 0 && CanFire())
         {
             _lastFire = Time.timeSinceLevelLoad;
-            Debug.DrawRay(transform.position, _lastMovement.normalized * settings.range, Color.red, .5f);
-            RaycastHit2D[] results = Physics2D.RaycastAll(transform.position, _lastMovement.normalized, settings.range);
-            if (results.Length > 0)
+
+            var b = _boxCollider2D.bounds;
+            var bottomLeft = _boxCollider2D.bounds.min;
+            var topRight = _boxCollider2D.bounds.max;
+            var bottomRight = new Vector3(topRight.x, bottomLeft.y);
+            var topLeft = new Vector3(bottomLeft.x, topRight.y);
+
+
+            GameObject meshObject = new GameObject("mesh");
+            meshObject.transform.parent = transform;
+
+            var mesh = new Mesh();
+            meshObject.AddComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Standard"));
+            meshObject.AddComponent<MeshFilter>().sharedMesh = mesh;
+            meshObject.AddComponent<PlayerAttackBehaviour>();
+            
+            mesh.Clear();
+
+            if (_lastDirection == Vector2.up)
             {
-                foreach (var result in results.Where(x => x.collider.tag == "Mover"))
-                {
-                    result.collider.GetComponentInParent<Mover>().Spook();
-                }
+                var topTopLeft = new Vector3(topLeft.x, topLeft.y + settings.range, topLeft.z);
+                var topTopRight = new Vector3(topRight.x, topRight.y + settings.range, topRight.z);
+
+                mesh.vertices = new Vector3[] {
+                    topLeft,
+                    topTopLeft,
+                    topTopRight,
+                    topRight
+                };
             }
+            else if (_lastDirection == Vector2.left)
+            {
+                var bottomLeftLeft = new Vector3(bottomLeft.x - settings.range, bottomLeft.y, bottomLeft.z);
+                var topLeftLeft = new Vector3(topLeft.x - settings.range, topLeft.y, topLeft.z);
+
+                mesh.vertices = new Vector3[]
+                {
+                    bottomLeft,
+                    bottomLeftLeft,
+                    topLeftLeft,
+                    topLeft
+                };
+            }
+            else if (_lastDirection == Vector2.right)
+            {
+                var topRightRight = new Vector3(topRight.x + settings.range, topRight.y, topRight.z);
+                var bottomRightRight = new Vector3(bottomRight.x + settings.range, bottomRight.y, bottomRight.z);
+
+                mesh.vertices = new Vector3[]
+                {
+                    topRight,
+                    topRightRight,
+                    bottomRightRight,
+                    bottomRight
+                };
+            }
+            else if (_lastDirection == Vector2.down)
+            {
+                var bottomBottomRight = new Vector3(bottomRight.x, bottomRight.y - settings.range, bottomRight.z);
+                var bottomBottomLeft = new Vector3(bottomLeft.x, bottomLeft.y - settings.range, bottomLeft.z);
+
+                mesh.vertices = new Vector3[]
+                {
+                    bottomRight,
+                    bottomBottomRight,
+                    bottomBottomLeft,
+                    bottomLeft
+                };
+            }
+
+            mesh.triangles = new int[] { 0, 1, 3, 3, 1, 2 };
+            mesh.RecalculateNormals();
+
+            var boxCollider = meshObject.AddComponent<BoxCollider2D>();
+            boxCollider.isTrigger = true;
+
+            Destroy(meshObject, .3f);
         }
 
         if (movement != Vector2.zero)
         {
+            _lastDirection = direction;
             _lastMovement = movement;
         }
     }
@@ -62,5 +137,25 @@ public class Player : MonoBehaviour
     private void OnGUI()
     {
         GUI.TextArea(new Rect(0, 0, 200, 40), "Hoz: " + System.Math.Round(Input.GetAxis("Horizontal"), 3).ToString("#.###") + " ||| Vert: " + System.Math.Round(Input.GetAxis("Vertical"), 3).ToString("#.###") + " ||| Cooldown: " + CanFire());
+    }
+
+    private Vector2 GetDirection(Vector2 v)
+    {
+        var compass = new Vector2[] { Vector2.left, Vector2.right, Vector2.up, Vector2.down };
+
+        var maxDot = -Mathf.Infinity;
+        var ret = Vector3.zero;
+
+        foreach (var dir in compass)
+        {
+            var t = Vector3.Dot(v, dir);
+            if (t > maxDot)
+            {
+                ret = dir;
+                maxDot = t;
+            }
+        }
+
+        return ret;
     }
 }
---
+++ Assets/Scripts/PlayerAttackBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttackBehaviour : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Mover")
+        {
+            collision.GetComponentInParent<Mover>().Spook();
+        }
+    }
+}
