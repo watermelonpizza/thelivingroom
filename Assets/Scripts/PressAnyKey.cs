b30f9d161e34c0009528f92067958bcfbce9669b
anthony-iob <anthony.iob7@gmail.com>
2019-01-27 11:05:47 +1100

    Coded a pressanykey function and implemented the functionality into the Title Screen

---
+++ Assets/Scripts/PressAnyKey.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PressAnyKey : MonoBehaviour {
+
+    private float delay = 0;
+    public float delayTime = 3;
+    public string sceneName;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (delay < delayTime)
+        {
+            delay += Time.deltaTime;
+            Debug.Log(delay);
+        }
+        else if (Input.anyKeyDown)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+
+	}
+}
