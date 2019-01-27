2506baaa4ad26f0c58692db6fefb5a9659b281fc
JeremyBurns <soundconclusion@gmail.com>
2019-01-27 13:34:20 +1100

    audio for buttons

--- Assets/Scripts/PressAnyKey.cs
+++ Assets/Scripts/PressAnyKey.cs
@@ -8,10 +8,11 @@ public class PressAnyKey : MonoBehaviour {
     private float delay = 0;
     public float delayTime = 3;
     public string sceneName;
-
+    AudioSource audioSource;
+    
 	// Use this for initialization
 	void Start () {
-		
+        audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -24,6 +25,11 @@ public class PressAnyKey : MonoBehaviour {
         }
         else if (Input.anyKeyDown)
         {
+            if (audioSource)
+            {
+                audioSource.Play();
+            }
+                
             SceneManager.LoadScene(sceneName);
         }
 
